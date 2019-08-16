e6dd46d4f76478865da7fc04cc65ba975e9a7028
Trionfante <melv_hartan@live.com>
2019-08-17 01:29:09 +0700

    comment out extra

--- exercise-1.js
+++ exercise-1.js
@@ -1,4 +1,25 @@
 function angkaPrima(angka) {
+    if (angka < 2) {
+        return false;
+    } else {
+        for (var i = 2; i < angka-1; i++) {
+            if (angka % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+  // TEST CASES
+  console.log(angkaPrima(3)); // true
+  console.log(angkaPrima(7)); // true
+  console.log(angkaPrima(6)); // false
+  console.log(angkaPrima(23)); // true
+  console.log(angkaPrima(33)); // false
+
+/*
+function angkaPrima(angka) {
     debugger
     if (angka < 2) {
         return '0' + angka + ' is not prime'
@@ -20,14 +41,8 @@ function angkaPrima(angka) {
     }
   }
   
-  // TEST CASES
-//   console.log(angkaPrima(3)); // true
-//   console.log(angkaPrima(7)); // true
-//   console.log(angkaPrima(6)); // false
-//   console.log(angkaPrima(23)); // true
-//   console.log(angkaPrima(33)); // false
+  for (var count = 0; count < 100; count++) {
+      console.log(angkaPrima(count));
+  }
 
-
-for (var count = 0; count < 100; count++) {
-    console.log(angkaPrima(count));
-}
+  */
